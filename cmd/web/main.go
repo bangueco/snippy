f4c0dee2f3b414c0d5125bfde39231c564dc6a53
bangueco <121280104+bangueco@users.noreply.github.com>
2025-03-04 15:13:13 +0800

    feat: add database initialization

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
+
+	"github.com/bangueco/snippy/internal/models"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger        *slog.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "root:mysql@tcp(localhost:3306)/snippetbox?parseTime=true", "MYSQL data source name")
 
 	flag.Parse()
 
@@ -22,13 +30,44 @@ func main() {
 		AddSource: true,
 	}))
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
 	app := &application{
-		logger: logger,
+		logger:        logger,
+		snippets:      &models.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
 
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
